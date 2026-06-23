df47b7343b35ba3d62d8400717e6e4545e0f7e65
nayan <nayanbajpai653@gmail.com>
2026-06-23 17:01:17 +0530

    Update first_code.py

--- first_code.py
+++ first_code.py
@@ -7,5 +7,5 @@ import tensorflow # external library '''
 print("Hello World")
 print(5)  
 print("Hey i am a \'good boy\' and\nthis viewer is also a good boy/girl") # \n , \ is a escape sequence char 
-
+print(56*52)
 print("hello", 6, 8 ,sep="-", end="bye") # sep is a seprator end is what to print at the end
