c69a470554e9c648b882b3af742e8df57942e629
nayan <nayanbajpai653@gmail.com>
2026-06-23 16:46:16 +0530

    Create first_code.py

---
+++ first_code.py
@@ -0,0 +1,11 @@
+''' import pandas # built in library
+import tensorflow # external library '''
+
+# # , ''' , """ is use to add comments in py
+# # is single line comment
+# ''' , """ for multiline comment
+print("Hello World")
+print(5)  
+print("Hey i am a \'good boy\' and\nthis viewer is also a good boy/girl") # \n , \ is a escape sequence char 
+
+print("hello", 6, 8 ,sep="-", end="bye") # sep is a seprator end is what to print at the end
