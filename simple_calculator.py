5514ca4e8d8b49d715f9ab0a7015ea9010acbe10
nayan <nayanbajpai653@gmail.com>
2026-06-25 23:10:03 +0530

    Create simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,11 @@
+# simple calculator for two numbers
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
+print("Floor Division: ", num1 // num2)
+print("Exponentiation: ", num1 ** num2)
+print("Modulus: ", num1 % num2)
