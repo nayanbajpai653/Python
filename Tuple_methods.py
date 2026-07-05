b7182e80e4b1e707348c71b10e691efcb954b2cd
nayan <nayanbajpai653@gmail.com>
2026-07-05 12:41:41 +0530

    Create Tuple_methods.py

---
+++ Tuple_methods.py
@@ -0,0 +1,7 @@
+# Tuple methods 
+t = (0, 858, 4, 1, 8, 2, 1, 63, 0)
+
+print(t.count(0)) # Returns the number of times 0 appears in the tuple
+print(t.index(1)) # Returns the index of the first occurrence of 1 in the tuple
+print(t.index(1, 3)) # Returns the index of the first occurrence of 1 in the tuple starting from index 3
+
