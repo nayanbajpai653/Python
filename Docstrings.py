1c199408503f2536d8d47d7d919e79385877b066
nayan <nayanbajpai653@gmail.com>
2026-07-07 12:43:20 +0530

    Create Docstrings.py

---
+++ Docstrings.py
@@ -0,0 +1,8 @@
+# Docstrings
+def square(n):
+    '''Takes a number n and returns its square.'''  # this is docstring and it is placed right under the function 
+    print(n ** 2)
+
+square(5)
+print(square.__doc__)  # Output: Takes a number n and returns its square.)
+
